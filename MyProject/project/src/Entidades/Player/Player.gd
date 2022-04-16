2defc10ee066b190e9fec4c71d49f769f35e3712
Ftcv <64758840+Ftcv@users.noreply.github.com>
2022-04-15 23:38:50 -0300

    Várias mudanças
    Mudanças principalmente no movimento do player

--- MyProject/project/src/Entidades/Player/Player.gd
+++ MyProject/project/src/Entidades/Player/Player.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
-export var walk_speed = 200
-export var gravity = 150
-export var jump_speed = - 100
+var max_walk_speed = 200
+var gravity = 150
+var jump_speed = - 100
+var acceleration = 20 # quanto menor maior a inercia
+var friction = 0.25 # quanto menor mais o player desliza
 var is_alive = true
-var velocity = Vector2(0,0)
+var motion = Vector2(0,0)
 var state
 enum {CORRENDO,IDLE,ANDANDO,NADANDO,SWING_ROPE,GLIDING,DESLIZANDO,PULANDO}
 
@@ -34,22 +36,25 @@ func _physics_process(delta):
 	
 	
 	if is_alive == true:
-		if velocity.x != 0:
+		
+		if motion.x != 0:
 			$AnimationPlayer.play("Correndo")
 		else:
 			$AnimationPlayer.play("Respirando")
-		move_and_slide(velocity, Vector2(0, -1))
-		velocity.y += delta * gravity
-		if Input.is_action_pressed("ui_up"):
+		move_and_slide(motion, Vector2(0, -1))
+		motion.y += delta * gravity
+		if Input.is_action_just_pressed("ui_up"):
 			if is_on_floor():
-				velocity.y = jump_speed
+				motion.y = jump_speed
 		if Input.is_action_pressed("ui_left"):
 			$Sprite.flip_h = true
-			velocity.x = -walk_speed
+			motion.x = max(motion.x-acceleration,-max_walk_speed)
 		elif Input.is_action_pressed("ui_right"):
 			$Sprite.flip_h = false
-			velocity.x = walk_speed
+			motion.x = min(motion.x+acceleration,max_walk_speed)
 		else:
-			velocity.x = 0
+			motion.x = lerp(motion.x,0,friction)
+			if abs(motion.x) < 1: 
+				motion.x = 0
 
 
--- MyProject/project/src/Gameengine/SceneManager.gd
+++ MyProject/project/src/Gameengine/SceneManager.gd
@@ -3,3 +3,8 @@ extends Node2D
 export (NodePath) var player_path
 onready var player = get_node(player_path)
 
+#export (NodePath) var game_over_path
+#onready var game_over_ui = get_node(game_over_path)
+#
+#func _ready():
+#	game_over_ui.visible = false
